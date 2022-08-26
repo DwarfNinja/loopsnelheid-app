63b3e01b0f2d3fcea8af4e93a114b405784b0226
DwarfNinja <cenduroyib@gmail.com>
2022-08-26 22:24:16 +0200

    Refactor: rebase GenderToggle to seperate file

--- lib/views/register/register_details.dart
+++ lib/views/register/register_details.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'package:flutter_switch/flutter_switch.dart';
 import 'package:intl/intl.dart';
 
 import 'package:loopsnelheidapp/models/user.dart';
@@ -10,6 +9,8 @@ import 'package:loopsnelheidapp/widgets/register/date_input.dart';
 import 'package:loopsnelheidapp/widgets/register/form_button.dart';
 import 'package:loopsnelheidapp/widgets/register/input_field.dart';
 import 'package:loopsnelheidapp/widgets/register/register_base.dart';
+import 'package:loopsnelheidapp/widgets/gender_toggle.dart';
+
 
 import 'package:loopsnelheidapp/services/shared_preferences_service.dart';
 
@@ -111,62 +112,6 @@ class _RegisterDetailsState extends State<RegisterDetails> {
               ]
           )
         ]
-    );
-  }
-}
-
-class GenderToggle extends StatefulWidget {
-  final Function(bool) onToggle;
-  final bool value;
-  final TextStyle? headerStyle;
-  final Size switchSize;
-  final double toggleSize;
-
-  const GenderToggle({
-    Key? key,
-    required this.onToggle,
-    required this.value,
-    this.headerStyle,
-    this.switchSize = const Size(120, 40),
-    this.toggleSize = 35
-  }) : super(key: key);
-
-  @override
-  State<GenderToggle> createState() => _GenderToggleState();
-}
-
-class _GenderToggleState extends State<GenderToggle> {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Align(
-          alignment: Alignment.centerLeft,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: Text(
-                "Geslacht",
-                style: widget.headerStyle ?? app_theme.textTheme.headline6!.copyWith(color: app_theme.black)
-            ),
-          ),
-        ),
-        const SizedBox(width: 25),
-        FlutterSwitch(
-          width: widget.switchSize.width,
-          height: widget.switchSize.height,
-          valueFontSize: 18,
-          toggleSize: widget.toggleSize,
-          value: widget.value,
-          activeText: "Vrouw",
-          inactiveText: "Man",
-          activeIcon: const Icon(Icons.female),
-          inactiveIcon: const Icon(Icons.male),
-          activeColor: app_theme.red,
-          inactiveColor: app_theme.blue,
-          showOnOff: true,
-          onToggle: widget.onToggle,
-        ),
-      ],
     );
   }
 }
