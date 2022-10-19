d7b480ea88a2108a7b9a48810bf79de4287ad421
DwarfNinja <cenduroyib@gmail.com>
2022-10-20 00:54:28 +0200

    Feat: add background activity recognition and overhaul measuring code
    Add background service
    Add activity service to background service to let it run in the background
    Refactor location service code to background service
    Generally refactor and optimized measuring code

--- lib/services/measure/location_service.dart
+++ lib/services/measure/location_service.dart
@@ -1,10 +1,7 @@
 import 'dart:async';
-import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:flutter_background_service_android/flutter_background_service_android.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:geolocator/geolocator.dart';
 
 import 'package:loopsnelheidapp/models/measure.dart';
@@ -15,6 +12,8 @@ class LocationService {
   static List<Measure> measureList = [];
 
   static bool isServiceRunning = false;
+
+  static StreamSubscription<Position>? positionStream;
 
   static const LocationSettings locationSettings = LocationSettings(
       accuracy: LocationAccuracy.best,
@@ -37,82 +36,53 @@ class LocationService {
     return Geolocator.openAppSettings();
   }
 
-  static Future<void> initializeService() async {
-    final backgroundService = FlutterBackgroundService();
-    await backgroundService.configure(
-      androidConfiguration: AndroidConfiguration(
-        onStart: onLocationStart,
-        autoStart: false,
-        isForegroundMode: true,
-      ),
-      iosConfiguration: IosConfiguration(
-        autoStart: false,
-        onForeground: onLocationStart,
-        onBackground: onIosBackground,
-      ),
-    );
-  }
+  static void runLocationService() {
+    positionStream = Geolocator.getPositionStream(locationSettings: locationSettings)
+        .handleError(onPositionError)
+        .listen(onReceivePosition);
 
-  static startLocationService() async {
-    await initializeService();
-    final service = FlutterBackgroundService();
-    service.startService();
     isServiceRunning = true;
+
+    Timer.periodic(const Duration(seconds: 30), (timer) {
+      uploadData();
+      if (isServiceRunning == false) {
+        timer.cancel();
+      }
+    });
   }
 
-  static stopLocationService() async {
-    final service = FlutterBackgroundService();
-    service.invoke("stopService");
+  static void stopLocationService() {
+    cancelMeasuring();
+    uploadData();
     isServiceRunning = false;
   }
 
-  @pragma('vm:entry-point')
-  static void onLocationStart(ServiceInstance service) async {
-    DartPluginRegistrant.ensureInitialized();
-
-    await dotenv.load();
-
-    if (service is AndroidServiceInstance) {
-      service.on('setAsForeground').listen((event) {
-        service.setAsForegroundService();
-      });
-
-      service.on('setAsBackground').listen((event) {
-        service.setAsBackgroundService();
-      });
-    }
-
-    service.on('stopService').listen((event) {
-      service.stopSelf();
-    });
-
-    Timer.periodic(const Duration(seconds: 3), (timer) async {
-      if (service is AndroidServiceInstance) {
-        service.setForegroundNotificationInfo(
-            title: "Loopsnelheid",
-            content: "Metingen aan het uitvoeren..."
-        );
-      }
-    });
-
-    Stream<Position> positionStream =
-    Geolocator.getPositionStream(locationSettings: locationSettings);
-    positionStream.listen((Position? position) {
-      var speed = position?.speed ?? 0.0;
-      var convertedSpeed = MeasureService.convertMsToKmh(speed);
-      Measure measure = Measure(DateTime.now().toIso8601String(), convertedSpeed);
+  static void onReceivePosition(Position position) {
+    var speed = position.speed;
+    if (speed != 0) { //TODO: ADD MARGIN
+      double convertedSpeed = MeasureService.convertMsToKmh(speed);
+      Measure measure = Measure(
+          DateTime.now().toIso8601String(), convertedSpeed);
       measureList.add(measure);
-    });
+    }
+  }
 
-    Timer.periodic(const Duration(seconds: 30), (timer) {
-      if (measureList.isNotEmpty) {
-        MeasureService.storeMeasures(measureList);
-      }
-    });
+  static void uploadData() {
+    if (measureList.isNotEmpty) {
+      MeasureService.storeMeasures(measureList);
+    }
+  }
+
+  static void cancelMeasuring() {
+   positionStream?.cancel();
   }
 
   static bool onIosBackground(ServiceInstance service) {
     WidgetsFlutterBinding.ensureInitialized();
     return true;
+  }
+
+  static void onPositionError(dynamic error) {
+    print('Catch Error >> $error');
   }
 }
